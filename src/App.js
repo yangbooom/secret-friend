bd18ffd0af86074e578ca53c0877b1db01713c84
jinhosoo10 <jinhosoo10@gmail.com>
2020-06-21 12:01:44 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           and save to reload.
-        </p>
+        </p> hahahahahaha
         <a
           className="App-link"
           href="https://reactjs.org"
