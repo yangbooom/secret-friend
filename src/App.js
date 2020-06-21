ef24f2b8392c2015c0d1d9cbecdddfec847de709
Pureun Kim <nononi0811@gmail.com>
2020-06-21 11:45:51 +0900

    cc

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit hosoo and save to reload.
+          Edit pureun and save to reload.
         </p>
         <a
           className="App-link"
