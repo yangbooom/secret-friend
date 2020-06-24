dda5626e2cbd2a736ab7f49a66bb09ec23c67c84
yb <kimuyb@snu.ac.kr>
2020-06-25 00:47:05 +0900

    Update Api.js

--- src/Api.js
+++ src/Api.js
@@ -1,33 +1,29 @@
-var myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
+const myHeaders = new Headers();
+myHeaders.append('Content-Type', 'application/json');
 
-var raw = {
-  apiKey: "dcf102a946024eafb1c3d61cbdba3c47",
-  bankName: "기업",
-  bankAccountNo: "21604828802016",
-  amount: 15000,
-  message: "aaaaaa???"
+const toss = (amount) => {
+  const raw = {
+    apiKey: 'e449748e546f4dfab7b3ce74d510accc',
+    bankName: '카카오뱅크',
+    bankAccountNo: '3333070047832',
+    amount,
+    message: '비밀친구를 만나시겠습니까?',
+  };
+
+  const JSONraw = JSON.stringify(raw);
+
+  const requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: JSONraw,
+    redirect: 'follow',
+  };
+
+  return fetch(
+    'https://toss.im/transfer-web/linkgen-api/link',
+    requestOptions,
+  ).then((response) => response.text());
 };
 
-export default class Api {
-  toss(user) {
-    raw.bankName = user.bankName;
-    raw.bankAccountNo = user.bankAccountNo;
-    raw.amount = user.ordermoney;
-    raw.bankName = user.bankName;
+export default toss;
 
-    var JSONraw = JSON.stringify(raw);
-
-    var requestOptions = {
-      method: "POST",
-      headers: myHeaders,
-      body: JSONraw,
-      redirect: "follow"
-    };
-
-    return fetch(
-      "https://toss.im/transfer-web/linkgen-api/link",
-      requestOptions
-    ).then(response => response.text());
-  }
-}
