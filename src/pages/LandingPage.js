56f4e94dc59bde906af42024edb177ac90f62447
yb <kimuyb@snu.ac.kr>
2020-06-24 13:58:54 +0900

    add material ui

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ReactComponent as LandingImage } from '../img/landing_image.svg';
+import { ReactComponent as Logo } from '../img/logo.svg';
 import {
   signInWithGoogle, auth, logout, writeUserData, uiLoader,
 } from '../firebase.util';
@@ -7,7 +8,8 @@ import {
 const LandingPage = () => (
   <div style={{'text-align': 'center', 'marginTop': '50px'}}>
     <LandingImage style={{height: '500px' ,}}/>
-    <h3></h3>
+    <h3>야식, 아직도 혼자먹니?<br/>나의 야식 메이트 찾기,</h3>
+    <Logo style={{width: '300px'}}/>
   </div>
 );
 
