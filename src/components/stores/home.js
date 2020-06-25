b9e8c0e01d07bd087524834b1d6d64de72033dea
yb <kimuyb@snu.ac.kr>
2020-06-25 19:22:15 +0900

    home error revision

--- src/components/stores/home.js
+++ src/components/stores/home.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react'; 
+import React, { Component } from 'react';
 
-class Home extends Component { 
+class Home extends Component {
 
-    render() { 
-        this.loginCheck()
-        return ( 
-            <div style={{textAlign:'center'}}> 
-                <h2>오늘 뭐먹지?</h2>
-                <h3>메뉴를 골라주세요</h3>
-                <img src="https://i.ibb.co/YB6Qsp1/whichone.png" width="300px" alt="home"></img>
-            </div> 
-        ); 
-    } 
-} 
+  render() {
+    // this.loginCheck();
+    return (
+      <div style={{ textAlign: 'center' }}>
+        <h2>오늘 뭐먹지?</h2>
+        <h3>메뉴를 골라주세요</h3>
+        <img src="https://i.ibb.co/YB6Qsp1/whichone.png" width="300px" alt="home" />
+      </div>
+    );
+  }
+}
 
 export default Home;
